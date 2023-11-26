cb7e16b2fbc61d5993cff27c51f3d5758b3196a3
Bimsara Walallawita <bwalallawita@gmail.com>
2023-11-26 12:56:16 +0530

    JWT Auth

--- src/main/java/com/ijse/database/repository/UserRepository.java
+++ src/main/java/com/ijse/database/repository/UserRepository.java
@@ -4,8 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ijse.database.entity.User;
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     //Custom Queries
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/ijse/database/security/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.ijse.database.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig {
+    
+}
