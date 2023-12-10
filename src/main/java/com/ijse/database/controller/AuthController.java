40c82b24e51f44af8069d185000628a238bd62aa
Bimsara Walallawita <bwalallawita@gmail.com>
2023-12-10 12:46:28 +0530

    auth completed

--- src/main/java/com/ijse/database/controller/AuthController.java
+++ src/main/java/com/ijse/database/controller/AuthController.java
@@ -1,13 +1,71 @@
 package com.ijse.database.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ijse.database.dto.LoginDTO;
+import com.ijse.database.entity.User;
+import com.ijse.database.repository.UserRepository;
+import com.ijse.database.security.jwt.JwtUtils;
+import com.ijse.database.service.UserService;
 
 @RestController
 public class AuthController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
     
-    @GetMapping("/auth/login") 
-    public String login() {
-        return "Login works without authentication";
+    @PostMapping("/auth/login") 
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
+
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtUtils.generateJwtToken(authentication);
+
+        return ResponseEntity.ok().body(token);
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        
+        if(userRepository.existsByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().body("Username is already in use");
+        }
+
+        if(userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.badRequest().body("This email is being used");
+        }
+
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return ResponseEntity.ok(userService.createUser(newUser));
     }
 }
